5c88005d1cc3a82ec4a31f70b7fb80cdb079cfe0
debian <debian@debian.msibrahim>
2021-01-06 14:11:05 +0200

    your comment

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -512,11 +512,11 @@ if not app.debug:
 
 # Default port:
 if __name__ == '__main__':
-    app.run()
+   app.run(host='192.168.1.21')
 
 # Or specify port manually:
 '''
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='192.168.1.21', port=port)
 '''
